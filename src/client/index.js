8029deb913710618e8c337d278bd152705b92d5a
Mark Goudie <goudie.mark@gmail.com>
2021-10-09 15:31:32 +1100

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+import "./styles/general.scss";
+import "./styles/style.scss";
+// import "./styles/form.scss";
+// import "./styles/header.scss";
+// import "./styles/resets.scss";
+
+console.log(checkForName);
+
+export { checkForName, handleSubmit };
