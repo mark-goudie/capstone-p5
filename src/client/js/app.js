6cdf37ef80505982541902bc11f1a7e9fdd25025
Mark Goudie <goudie.mark@gmail.com>
2021-10-12 07:34:25 +1100

    api keys added

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,7 +3,7 @@
 // URL endpoint (Zip) //
 
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = "refer .env";
+const apiKey = "";
 
 // Create a new date instance dynamically with JS //
 
