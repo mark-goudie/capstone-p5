e2aa81010ac98e654a1dbe47cfb865d3afd4d8b8
Mark Goudie <goudie.mark@gmail.com>
2021-10-26 10:26:39 +1100

    update tests

---
+++ __test__/app.js
@@ -0,0 +1,16 @@
+import { getCountdown } from "./getCountdown";
+
+test("Testing the handleSubmit() function", () => {
+  expect(getCountdown).toBeDefined();
+});
+
+import { getTripLength } from "./getTripLength";
+
+test("Testing the handleSubmit() function", () => {
+  expect(getTripLength).toBeDefined();
+});
+import { updateUI } from "./updateUI";
+
+test("Testing the handleSubmit() function", () => {
+  expect(updateUI).toBeDefined();
+});
